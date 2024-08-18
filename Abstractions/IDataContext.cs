8d7f316cec0c1c0144d1237fcd18a8ad0a4d9939
Simon Halsey <simon@thehalseys.co.uk>
2024-08-18 23:25:46 +0100

    moved sqlite to assembly

--- Abstractions/IDataContext.cs
+++ Abstractions/IDataContext.cs
@@ -6,21 +6,17 @@ namespace Timeoff
 {
     public interface IDataContext
     {
-        //DbSet<Audit> Audits { get; set; }
-        //DbSet<PublicHoliday> PublicHolidays { get; set; }
+        DbSet<Company> Companies { get; }
 
-        //DbSet<Comment> Comments { get; set; }
-        DbSet<Company> Companies { get; set; }
-
-        DbSet<Team> Teams { get; set; }
-        DbSet<EmailAudit> EmailAudits { get; set; }
-        DbSet<UserFeed> Feeds { get; set; }
-        DbSet<Leave> Leaves { get; set; }
-        DbSet<LeaveType> LeaveTypes { get; set; }
-        DbSet<Schedule> Schedules { get; set; }
-        DbSet<UserAllowanceAdjustment> UserAllowanceAdjustments { get; set; }
-        DbSet<User> Users { get; set; }
-        DbSet<Calendar> Calendar { get; set; }
+        DbSet<Team> Teams { get; }
+        DbSet<EmailAudit> EmailAudits { get; }
+        DbSet<UserFeed> Feeds { get; }
+        DbSet<Leave> Leaves { get; }
+        DbSet<LeaveType> LeaveTypes { get; }
+        DbSet<Schedule> Schedules { get; }
+        DbSet<UserAllowanceAdjustment> UserAllowanceAdjustments { get; }
+        DbSet<User> Users { get; }
+        DbSet<Calendar> Calendar { get; }
 
         IDbContextTransaction BeginTransaction();
 
